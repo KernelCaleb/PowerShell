ac9ed882b82d2dc45be4b288b1eb7d0eea0f9853
KernelCaleb <69069150+KernelCaleb@users.noreply.github.com>
2021-01-07 21:20:25 -0700

    Update Exchange_ConnectionScript.ps1

--- Exchange_ConnectionScript.ps1
+++ Exchange_ConnectionScript.ps1
@@ -1,3 +1,8 @@
-﻿$UserCredential = Get-Credential
+# Exchange ConnectionScript
+
+# KernelCaleb
+# 2021-01-07
+
+$UserCredential = Get-Credential
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
 Import-PSSession $Session -DisableNameChecking
