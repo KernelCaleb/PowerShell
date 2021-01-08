260c28aeab59e8fc44faf39d7bc757cf7b6ed704
KernelCaleb <69069150+KernelCaleb@users.noreply.github.com>
2021-01-07 21:14:07 -0700

    Update AAD_GetGuestAccounts.ps1

--- AAD_GetGuestAccounts.ps1
+++ AAD_GetGuestAccounts.ps1
@@ -1,1 +1,3 @@
+# AAD Get-GuestAccounts
+
 Get-AzureADUser -Filter "Usertype eq 'Guest'” | Export-csv -path "C:\Users\csnethen\OneDrive - Professional Case Management\Documents\IT\PowerShell\Output\AzAD_Guest_2020-12-15.csv"
