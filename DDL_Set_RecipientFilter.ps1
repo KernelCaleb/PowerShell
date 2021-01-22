2a45099e1fec26271d70a4cb36a6a468dbc714ca
KernelCaleb <69069150+KernelCaleb@users.noreply.github.com>
2021-01-21 21:43:10 -0700

    Update DDL_Set_RecipientFilter.ps1

--- DDL_Set_RecipientFilter.ps1
+++ DDL_Set_RecipientFilter.ps1
@@ -1,3 +1,4 @@
-﻿Set-DynamicDistributionGroup -Identity “ClinicalOperationsCM@procasemanagement.com” -RecipientFilter {((RecipientType -eq 'UserMailbox') -and ((Department -eq 'Case Management') -and (Title -eq 'Case Manager')))}
-Set-DynamicDistributionGroup -Identity “ClinicalOperationsSCM@procasemanagement.com” -RecipientFilter {((RecipientType -eq 'UserMailbox') -and ((Department -eq 'Clinical Administration') -and (Title -eq 'Senior Case Manager')))}
-Set-DynamicDistributionGroup -Identity “AllClinicalOperationsAdmin@procasemanagement.com” -RecipientFilter {((RecipientType -eq 'UserMailbox') -and (((Department -eq 'Clinical Administration') -and ((Title -ne 'Case Manager') -and (Title -ne 'Senior Case Management')) -or (Title -eq 'Physical Therapist Lead'))))}
+$DDL = "..."
+$MemberRule = "..."
+
+Set-DynamicDistributionGroup -Identity "$DDL" -RecipientFilter {(RecipientType -eq 'UserMailbox') -and ($MemberRule)}
